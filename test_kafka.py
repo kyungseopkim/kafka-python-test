d28cf2a7acf542de2a1af7439449fffc34c5be8a
Jerry Kim <jerrykim@lucidemotors.com>
2019-11-12 10:10:21 -0800

    add kafka-test-job.yaml for K8s

--- test_kafka.py
+++ test_kafka.py
@@ -4,8 +4,10 @@ import configparser
 config = configparser.ConfigParser()
 
 config.read('kafka.ini')
-bootstrap = config['kafka']['bootstrap_hosts']
-consumer = kafka.KafkaConsumer(group_id='test', bootstrap_servers=[bootstrap])
+bootstrap = config['kafka']['bootstrap_hosts'].split(',')
+print(bootstrap)
+
+consumer = kafka.KafkaConsumer(group_id='test', bootstrap_servers=bootstrap, security_protocol="SSL")
 topics = consumer.topics()
 for topic in topics:
     print(topic)
